703b97d6448bdb9b60dc0a6105c9338b56e53cf5
Дмитрий Строкин < >
2016-08-11 21:31:53 +0300

    Загрузка моделей при запуске программы (коммит 4f82f45) вызывала блокировку БД

--- WarehouseBlockForms/Views/MainWindow.xaml.cs
+++ WarehouseBlockForms/Views/MainWindow.xaml.cs
@@ -55,7 +55,7 @@ namespace WarehouseBlockForms.Views
                 return;
             }
 
-            loadModels();
+            //loadModels();
 
             setWorkPages();
             setSettingsPages();
